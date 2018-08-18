fe967e89b8776c94469894fcab8fa6280c241891
Jake Lee <louisoel@gmail.com>
2018-08-17 18:16:42 -0700

    Index Page draft
    Planning on replacing the card deck.

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	var opts = {
+	  containerId: "sub",
+	  namespace: "sub",
+	  interval: 5000,
+	  speed: 200,
+	  mobileWidth: null,
+	  verbose: false,
+	  random: false,
+	  best: true,
+	  clearOriginalContent: false,
+	};
+	var opts2 = {
+	  containerId: "sub2",
+	  namespace: "sub2",
+	  interval: 5000,
+	  speed: 200,
+	  mobileWidth: null,
+	  verbose: false,
+	  random: false,
+	  best: true,
+	  clearOriginalContent: false,
+	};
+	var sub = new Sub([
+		"CoLiberate is how makers",
+		"CoLiberate is how educators",
+		"CoLiberate is how entrepeneurs",
+		"CoLiberate is how students",
+		"CoLiberate is how citizens",
+		"CoLiberate is how leaders",
+		"CoLiberate is how pilots"
+	], opts).run();
+	var sub2 = new Sub([
+		"build everything together.",
+		"build classrooms together.",
+		"build businesses together.",
+		"build projects together.",
+		"build communities together.",
+		"build futures together.",
+		"build drones together."
+	], opts2).run();
+});
+
+
